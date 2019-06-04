09f1d4f51f3ccbfc6a0aaae27f5f8ea2e0a50659
Ilya Z <ilyamz@gmail.com>
2019-06-03 23:48:24 -0700

    Add prometheus

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/huyamber/huyamber
 
 go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.2
+	github.com/prometheus/client_golang v0.9.3
+)
--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 
 	router := NewRouter()
+
+	router.Handle("/metrics", promhttp.Handler())
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
